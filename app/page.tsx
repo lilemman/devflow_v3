ddea38d3158fe42c35a918a613de0f7632f25cfd
Emmanuel Atuahene <62813726+lilemman@users.noreply.github.com>
2024-11-05 09:10:53 -0500

    ESLint and Prettier setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,5 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl text-violet-700 font-black">Welcome to DevFlow</h1>
+    <h1 className="text-3xl font-black text-violet-700">Welcome to DevFlow</h1>
   );
 }
