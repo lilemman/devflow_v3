10df4dd51b37b0c7dfd272ebcf64bc7564061bea
Emmanuel Atuahene <62813726+lilemman@users.noreply.github.com>
2024-11-05 10:00:06 -0500

    added assets and metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,20 +3,22 @@ import localFont from "next/font/local";
 import React from "react";
 import "./globals.css";
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
+const inter = localFont({
+  src: "./fonts/InterVF.ttf",
+  variable: "--font-inter",
+  weight: "100 200 300 400 500 700 800 900",
 });
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
+const SpaceGrotesk = localFont({
+  src: "./fonts/SpaceGrotesk.ttf",
+  variable: "--font-space-grotesk",
+  weight: "300 400 500 700 ",
 });
 
 export const metadata: Metadata = {
-  title: "DDevFlow 3.0",
-  description: "A better version of stack overflow",
+  title: "DevFlow 3.0",
+  description:
+    "A community-driven platform for asking and answering programming questions. Get help, share knowledge, and collaborate with developers from around the world. Explore topics in web development, mobile app development, algorithms, data structures, and more.",
+  icons: "/public/images/site-logo.svg",
 };
 
 export default function RootLayout({
@@ -27,7 +29,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${inter.className} ${SpaceGrotesk.variable} antialiased`}
       >
         {children}
       </body>
--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 export default function Home() {
   return (
-    <h1 className="text-3xl font-black text-violet-700">Welcome to DevFlow</h1>
+    <>
+      <h1 className="text-3xl font-black text-violet-700">
+        Welcome to DevFlow
+      </h1>
+      <h1 className="h1-bold">Welcome to default font</h1>
+      <h1 className="h1-bold font-space-grotesk">Welcome to Space Grotesk</h1>
+    </>
   );
 }
